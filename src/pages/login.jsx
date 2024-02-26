e45e930cf79b78a47ee78ff3adee76b3280ba0e3
Onixdrew <132929561+Onixdrew@users.noreply.github.com>
2024-02-26 20:49:49 +0000

    changes

---
+++ src/pages/login.jsx
@@ -0,0 +1,15 @@
+
+// import { Link } from 'react-router-dom'
+
+const Login = () => {
+  return (
+    <>
+      <h1>gggggggggggggg</h1>
+      <h1>gggggggggggggg</h1>
+      <h1>gggggggggggggg</h1>
+      <h1>gggggggggggggg</h1>
+    </>
+  )
+}
+
+export default Login
